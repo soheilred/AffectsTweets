8f3cb6323935d28a176624bf9963b2fe4d44b334
SHi ON <mail@shayanamani.com>
2017-11-01 02:59:16 -0400

    A5 changed

--- src/main/java/edu/unh/cs/ir/pa5/Assignment5.java
+++ src/main/java/edu/unh/cs/ir/pa5/Assignment5.java
@@ -14,6 +14,97 @@ import java.util.*;
 
 public class Assignment5 {
 
+    public void task1() {
+        String qId;
+        String dId;
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("RankLibOutputTask1"));
+
+            String[] runfileFuncs = {"outputs/pa5/bnn_bnn", "outputs/pa5/lnc_ltn", "outputs/pa5/LM_U", "outputs/pa5/U_JM"};
+
+            ArrayList<String> rankLibStr = new ArrayList<>();
+            int rank = 0;
+
+            double feature = 0;
+            String featureStr = "";
+            int target = 0;
+            qId = "Q";
+            for (int j = 0; j < 12; j++) {
+                dId = "D" + String.valueOf(j + 1);
+                for (int i = 0; i < runfileFuncs.length; i++) {
+                    rank = rankParser(runfileFuncs[i], dId, qId);
+                    if (rank > 0) {
+                        feature = (1.0 / (double) rank);
+                    } else {
+                        feature = 0;
+                    }
+                    featureStr = featureStr.concat(" " + (i + 1) + ":" + feature);
+                    target = targetParser("outputs/pa5/U_DS", qId, dId);
+                }
+                rankLibStr.add(target + " qid:" + qId + featureStr + " # " + dId);
+            }
+
+            for (String str : rankLibStr) {
+                bw.write(str + "\n");
+            }
+            bw.close();
+        } catch (Exception e) {
+            System.out.println("Exception caught." + e.toString() + "\n");
+        }
+
+
+    }
+
+    public void task2() {
+        String qId;
+        String dId;
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("RankLibOutput"));
+
+            String[] runfileFuncs = {"outputs/pa5/bnn_bnn", "outputs/pa5/lnc_ltn", "outputs/pa5/LM_U", "outputs/pa5/U_JM", "outputs/pa5/U_DS"};
+
+            ArrayList<String> rankLibStr = new ArrayList<>();
+            int rank = 0;
+            // read the queries' file
+            File fOutlines = new File("./test200/train.test200.cbor.outlines");
+            final FileInputStream fISOutlines = new FileInputStream(fOutlines);
+
+            // read the paragraphs' file
+            File fParags = new File("./test200/train.test200.cbor.paragraphs");
+            final FileInputStream fISParags = new FileInputStream(fParags);
+
+            double feature = 0;
+            String featureStr = "";
+            int target = 0;
+            for (Data.Page page : DeserializeData.iterableAnnotations(fISOutlines)) {
+                qId = page.getPageId();
+                for (Data.Paragraph paragraph : DeserializeData.iterableParagraphs(fISParags)) {
+                    dId = paragraph.getParaId();
+                    for (int i = 0; i < runfileFuncs.length; i++) {
+                        rank = rankParser(runfileFuncs[i], dId, qId);
+                        if (rank > 0) {
+                            feature = (1.0 / (double) rank);
+                        } else {
+                            feature = 0;
+                        }
+                        featureStr = featureStr.concat(" " + (i + 1) + ":" + feature);
+                        target = targetParser("./test200/train.test200.cbor.article.qrels", qId, dId);
+                    }
+                    rankLibStr.add(target + " qid:" + qId + featureStr + " # " + dId);
+                }
+            }
+
+            for (String str : rankLibStr) {
+                bw.write(str + "\n");
+            }
+            bw.close();
+        } catch (Exception e) {
+            System.out.println("Exception caught." + e.toString() + "\n");
+        }
+
+    }
 
     public int rankParser(String rf, String doc, String query) {
         int rank = 0;
@@ -59,54 +150,15 @@ public class Assignment5 {
     }
 
     public static void main(String[] args) throws FileNotFoundException, CborException {
-        String qId;
-        String dId;
-
+        int taskNumber = 1;
         Assignment5 a5 = new Assignment5();
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("RankLibOutput"));
 
-            String[] runfileFuncs = {"outputs/pa5/bnn_bnn", "outputs/pa5/lnc_ltn", "outputs/pa5/LM_U", "outputs/pa5/U_JM", "outputs/pa5/U_DS"};
+        if (taskNumber == 1) {
+            a5.task1();
+        } else {
+            a5.task2();
 
-            ArrayList<String> rankLibStr = new ArrayList<>();
-            int rank = 0;
-            // read the queries' file
-            File fOutlines = new File("./test200/train.test200.cbor.outlines");
-            final FileInputStream fISOutlines = new FileInputStream(fOutlines);
-
-            // read the paragraphs' file
-            File fParags = new File("./test200/train.test200.cbor.paragraphs");
-            final FileInputStream fISParags = new FileInputStream(fParags);
-
-            double feature = 0;
-            String featureStr = "";
-            int target = 0;
-            for (Data.Page page : DeserializeData.iterableAnnotations(fISOutlines)) {
-                qId = page.getPageId();
-                for (Data.Paragraph paragraph : DeserializeData.iterableParagraphs(fISParags)) {
-                    dId = paragraph.getParaId();
-                    for (int i = 0; i < 5; i++) {
-                        rank = a5.rankParser(runfileFuncs[i], dId, qId);
-                        if (rank > 0) {
-                            feature = (1.0 / (double) rank);
-                        } else {
-                            feature = 0;
-                        }
-                        featureStr = featureStr.concat(" " + i + 1 + ":" + feature);
-                        target = a5.targetParser("./train.test200.cbor.article.qrels", qId, dId);
-                    }
-                    rankLibStr.add(target + " qid:" + qId + featureStr + " # " + dId);
-                }
-            }
-
-            for (String str : rankLibStr) {
-                bw.write(str + "\n");
-            }
-            bw.close();
-        } catch (Exception e) {
-            System.out.println("Exception caught." + e.toString() + "\n");
         }
-
     }
 
 }
