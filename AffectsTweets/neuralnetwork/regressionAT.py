327f46ede1224730dba12b98928b961f48008239
soheil <soheilred@yahoo.com>
2017-11-24 23:25:53 -0500

    Adding SVR.

---
+++ AffectsTweets/neuralnetwork/regressionAT.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+
+# ########################################
+# Reading feature vectors
+features = np.matrix([])
+input = file('RankLib', 'r')
+lines = input.readlines()
+input.close()
+for line in lines:
+	variablesArray = line.split('\t')
+	# features = np.append(features, [[ variablesArray[2] , variablesArray[3] , variablesArray[4] ]], axis = 0)
+	print variablesArray[2]
+print features
+
+# #############################################################################
+# Generate sample data
+X = np.sort(5 * np.random.rand(40, 1), axis=0)
+y = np.sin(X).ravel()
+
+# #############################################################################
+# Add noise to targets
+y[::5] += 3 * (0.5 - np.random.rand(8))
+
+# #############################################################################
+# Fit regression model
+svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+svr_lin = SVR(kernel='linear', C=1e3)
+svr_poly = SVR(kernel='poly', C=1e3, degree=2)
+y_rbf = svr_rbf.fit(X, y).predict(X)
+y_lin = svr_lin.fit(X, y).predict(X)
+y_poly = svr_poly.fit(X, y).predict(X)
+
+# #############################################################################
+# Look at the results
+lw = 2
+plt.scatter(X, y, color='darkorange', label='data')
+plt.plot(X, y_rbf, color='navy', lw=lw, label='RBF model')
+plt.plot(X, y_lin, color='c', lw=lw, label='Linear model')
+plt.plot(X, y_poly, color='cornflowerblue', lw=lw, label='Polynomial model')
+plt.xlabel('data')
+plt.ylabel('target')
+plt.title('Support Vector Regression')
+plt.legend()
+plt.show()
